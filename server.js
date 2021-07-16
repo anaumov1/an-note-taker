37cf7327ac271a3172588c20e796bb834ff81f61
AlexN <82428461+anaumov1@users.noreply.github.com>
2021-07-16 12:06:49 -0500

    created the port

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmalRoutes');
 
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 3001;
